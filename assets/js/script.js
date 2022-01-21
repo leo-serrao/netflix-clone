731cdf49f9773ce6aceb065d9101aa241eff5ed6
leo-serrao <leoserraom@gmail.com>
2022-01-21 17:23:43 -0300

    navbar transformada em fixa, primeira parte do corpo da página criada, main-movie.21/01

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 function abrePesquisa() {
-  div.classList.toggle('ativado')
-  input.classList.toggle('ativada')
+  div.classList.toggle('ativa')
+  input.classList.toggle('ativo')
+  linha.classList.toggle('ativada')
 }
 
 const lupa = document.getElementById('lupa')
-const div = document.getElementById('icon')
-const input = document.getElementById('search')
+const div = document.getElementById('pesquisa')
+const input = document.getElementById('input')
+const linha = document.getElementById('linha')
 
 lupa.addEventListener('click', abrePesquisa)
